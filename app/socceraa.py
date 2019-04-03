4c700787049637808ee77a8b07b2ad77d4e7d606
westeale <westeale@students.zhaw.ch>
2019-04-03 14:46:02 +0200

    init project

---
+++ app/socceraa.py
@@ -0,0 +1,23 @@
+"""
+Entry point for the Soccer Advertisment Analyser
+"""
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--input', help="Set to images or name of video. Default is: images", default='images')
+parser.add_argument('--accuracy', help="Set accuracy of ad detection (low accuracy has highest performance)", default='high', choices=['low', 'medium', 'high'])
+parser.add_argument('-out', help="Show analysis during processing", action='store_true')
+
+
+def init():
+    args = parser.parse_args()
+    print(args.input)
+    print(args.accuracy)
+    print(args.out)
+
+
+if __name__ == "__main__":
+    init()
+    
