bf7c0652491e01db33259a406f4fda477e7c281a
westeale <westeale@students.zhaw.ch>
2019-04-22 11:46:21 +0200

    changing config

--- app/tmp.py
+++ app/tmp.py
@@ -1,21 +1,9 @@
-"""
-Helper Functions for Result generator
-"""
-import cv2 as cv
+import sys
+import time
 
-video = cv.VideoCapture('../data/in/videos/tracking1.mp4')
-fps = video.get(cv.CAP_PROP_FPS)
-ok, img = video.read()
+def countdown(n):
+    for x in reversed(range(n)):
 
-frame_size = img.shape[:2]
-size = (frame_size[1], frame_size[0])
-fourcc = cv.VideoWriter_fourcc(*'XVID')
-out = cv.VideoWriter('../data/out/test1.avi',fourcc, fps, size)
+        time.sleep(1)
 
-while ok:
-    ok, img = video.read()
-    out.write(img)
-
-video.release()
-out.release()
-
+countdown(60)
